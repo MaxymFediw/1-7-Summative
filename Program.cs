276a1a7421076ff424be87d48473d2374ddba36b
Maxym Fediw <362706764@GoTVDSB.ca>
2026-03-03 11:54:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1_7_Summative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maxym F.
+            
+            Part1();
+        }
+
+        public static void Part1() 
+        {
+            
+
+
+
+
+
+        }
+    }
+}
