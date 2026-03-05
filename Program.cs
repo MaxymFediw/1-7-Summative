8aa2b974245fcac18d1481a4b330644489dd2cd1
Maxym Fediw <362706764@GoTVDSB.ca>
2026-03-05 13:19:49 -0500

    Maybe almost done lowk idk

--- Program.cs
+++ Program.cs
@@ -27,9 +27,10 @@ namespace _1_7_Summative
 
             Console.WriteLine("Welcome to Rock, Paper, Scissors!");
             Console.WriteLine("You got lucky! You won $100 worth of plays. Awesome!");
-            Console.WriteLine("Write Rock, Paper, or Scissors when the pompt that sais Rock, Paper, Scissors, Shoot is shown.");
+            Console.WriteLine("Write Rock, Paper, or Scissors when the prompt that says Rock, Paper, Scissors, Shoot is shown.");
             Console.WriteLine("If you tie, You do not Gain Nor Lose any money.");
-            Console.WriteLine("If You Win, your cash will multiply by 3.2. If you lose, your cash will be divided by 3.2.");
+            Console.WriteLine("If You Win, your cash will multiply by 3.2, AND you'll get your bet back! If you guess incorrectly, you will lose your     bet.");
+            Console.WriteLine("If you run out of money, it's Game Over.");
             Console.WriteLine("Let's get started!");
 
             money = 100;
@@ -42,68 +43,89 @@ namespace _1_7_Summative
 
             while (!done) 
             {
+               
+
+                Console.WriteLine("You have " + money + " dollars.");
                 Console.WriteLine("How much do you want to bet?");
                 Double.TryParse(Console.ReadLine(), out bet);
-               
-                //FIX LOSING MONEY-DO NOT DIVIDE CUZ IT WILL NEVER HIT ZERO
+
+                money -= bet;
                 
                 randomPlay = generator.Next(1, 4);
 
+                if (bet <= 0) 
+                {
+
+                    Console.WriteLine("You think your funny? $1.");
+                    bet = 1;
+                }
+
+                //if (bet > money) 
+                //{
+                //    Console.WriteLine("Nuh uh, can't do that. ALL IN!");
+                //    bet = money;
+                //}
+                
+                // As you can see above, I did have a system for a max amount the player could bet, but after multiple attempts to troubleshoot, it did not work, and messed up the numbers, God knows how.
+               
                 Console.WriteLine("Rock, Paper, Scissors, Shoot!");
                 play = Console.ReadLine().ToLower();
                 Console.WriteLine();
 
+                
+
                 if (play == "rock" && randomPlay == 3) 
                 {
-                    Console.WriteLine("Nice one! Rock Beats Scissors. You now have " + (bet * 3.2).ToString("C"), "!");
-                    money += bet;
+                    Console.WriteLine("Nice one! Rock Beats Scissors. You gained " + (bet * 3.2).ToString("C"), " on top of your origional bet!");
+                     money += (bet * 3.2);
                 }
 
                 if (play == "rock" && randomPlay == 1) 
                 {
                     Console.WriteLine("Oh, A Tie! You still have " + bet.ToString("C"), ".");
-                    
+                    money += bet;
                 }
 
                 if (play == "rock" && randomPlay == 2) 
                 {
-                    Console.WriteLine("Oh, You lose! You now have " + (bet / 3.2).ToString("C"), ".");
-                    money -= bet;
+                    Console.WriteLine("Oh, You lose! You lost " + (bet).ToString("C"), ".");
+                     money -= bet;
                 }
 
                 if (play == "paper" && randomPlay == 3) 
                 {
-                    Console.WriteLine("Oh, You lose! You now have " + (bet / 3.2).ToString("C"), ".");
+                    Console.WriteLine("Oh, You lose! You lost " + (bet).ToString("C"), ".");
                     money -= bet;
                 }
 
                 if (play == "paper" && randomPlay == 1)
                 {
-                    Console.WriteLine("Nice one! Paper beats Rock! You now have " + (bet * 3.2).ToString("C"), ".");
-                    money += bet;
+                    Console.WriteLine("Nice one! Paper beats Rock! You gained " + (bet * 3.2).ToString("C"), " on top of your origional bet!");
+                    money += (bet * 3.2);
                 }
 
                 if (play == "paper" && randomPlay == 2)
                 {
                     Console.WriteLine("Oh, A Tie! You still have " + bet.ToString("C"), ".");
+                    money += bet;
                 }
 
                 if (play == "scissors" && randomPlay == 3)
                 {
                     Console.WriteLine("Oh, A Tie! You still have " + bet.ToString("C"), ".");
-
+                    money += bet;
                 }
 
                 if (play == "scissors" && randomPlay == 1)
                 {
-                    Console.WriteLine("Oh, You Lose! You now have " + (bet / 3.2).ToString("C"), ".");
+                    Console.WriteLine("Oh, You Lose! You lost " + (bet).ToString("C"), ".");
                     money -= bet;
                 }
 
                 if (play == "scissors" && randomPlay == 2)
                 {
-                    Console.WriteLine("Nice one! Scissors beats Paper! You now have " + (bet * 3.2).ToString("C"), ".");
-                    money += bet;
+                    Console.WriteLine("Nice one! Scissors beats Paper! You gained " + (bet * 3.2).ToString("C"), " on top of your origional bate!");
+                    money += (bet * 3.2);
                 }
 
                 Console.WriteLine("Would you like to continue playing? Yes or No?");
@@ -117,12 +139,12 @@ namespace _1_7_Summative
 
                 if (keepGoing == "No" || keepGoing == "no") 
                 {
-                    Console.WriteLine("Ok, Goodbye.");
+                    Console.WriteLine("Ok, Goodbye. All winnings have been added to you bank account.");
                     done = true;
               
                 }
 
-                if (money == 0)
+                if (money <= 0)
                 {
                     Console.WriteLine("You are out of money. Deposit more, or go home, you bum.");
                     done = true;
